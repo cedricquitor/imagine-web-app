8b405057cc211bf71c96798ab37382c598a73a63
cedricquitor <cedricquitor.dev@gmail.com>
2023-03-20 15:59:28 +0800

    Changed base URL to dev

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
       setIsLoading(true);
 
       try {
-        const response = await fetch("https://imaginkraf-web-service.onrender.com/8080/api/v1/post", {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setIsGeneratingImage(true);
-        const response = await fetch("https://imaginkraf-web-service.onrender.com//api/v1/dalle", {
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
       setIsLoading(true);
 
       try {
-        const response = await fetch("https://imaginkraf-web-service.onrender.com/api/v1/post", {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
